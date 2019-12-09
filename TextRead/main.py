caac16603ef987a8232d94919e33d48d4f89ff3c
D0kkaebi <rnlcksgdk955@naver.com>
2019-12-09 11:58:39 +0900

    Add files via upload

---
+++ TextRead/main.py
@@ -0,0 +1,35 @@
+import boto3
+
+# Document
+documentName = "text.jpg"
+text = []
+
+# Read document content
+with open(documentName, 'rb') as document:
+    imageBytes = bytearray(document.read())
+
+# Amazon Textract client
+textract = boto3.client('textract')
+
+# Call Amazon Textract
+response = textract.detect_document_text(Document={'Bytes': imageBytes})
+
+for item in response["Blocks"]:
+    if item["BlockType"] == "LINE":
+     text.append(item["Text"])
+
+polly_client = boto3.Session(
+                aws_access_key_id='AKIA6P6LV5FOKCJRP5SQ',
+                aws_secret_access_key='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',
+                region_name='us-west-2').client('polly')
+
+for i in range(0, len(text)):
+    response[i] = polly_client.synthesize_speech(VoiceId='Joanna',
+                OutputFormat='mp3',
+                Text = text[i])
+
+file = open('speech.mp3', 'wb')
+for i in range(0, len(text)):
+    file.write(response[i]['AudioStream'].read())
+
+file.close()
