6cbaebd713f4b3b78825dda6448a47ea48782daf
D0kkaebi <rnlcksgdk955@naver.com>
2020-01-14 21:31:38 +0900

    Roll a Ball Study

---
+++ UnityEngine/RollAndBall/Assets/PickUp.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    public Vector3 mTumble;
+    public GameController controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(mTumble * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            gameObject.SetActive(false);
+            controller.AddScore(1);
+        }
+    }
+}
---
+++ UnityEngine/RollAndBall/Assets/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody mRB;
+    public float Speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mRB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 power = new Vector3(horizontal * Speed, 0, vertical * Speed);
+        mRB.velocity = power;
+        //mRB.AddForce(power);
+        if(Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("Booster");
+        }
+    }
+}
